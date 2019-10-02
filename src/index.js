339ede736a22b7b468982fd42718068499775c81
rustem.mustafaiev <rustem@backendless.consulting>
2019-10-02 14:17:39 +0300

    Add an example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const {runInParallel, delay} = require('./utils/async')
 const {range} = require('./utils/array')
 
-
+/*
+Just an example of utilize parallel util
+ */
 async function main() {
+
 
     const stubTasks = range(50).map(n => () => delay(500).then(() => `#${n} completed`))
 
